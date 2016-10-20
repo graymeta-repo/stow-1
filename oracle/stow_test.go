777f5dd45333d94c4c6446a76a499183ac0f26bc
Piotr Rojek <piotrrojek@gmail.com>
2016-10-20 16:46:44 +0300

    Rebasing for open-source

---
+++ oracle/stow_test.go
@@ -0,0 +1,96 @@
+package swift
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+
+	"github.com/cheekybits/is"
+	"github.com/graymeta/stow"
+	"github.com/graymeta/stow/test"
+)
+
+var cfgUnmetered = stow.ConfigMap{
+	"username":               "aaron@graymeta.com",
+	"password":               "HPdq85BwQ66r",
+	"authorization_endpoint": "https://storage-a422618.storage.oraclecloud.com/auth/v1.0",
+}
+
+var cfgMetered = stow.ConfigMap{
+	"username":               "aaron@graymeta.com",
+	"password":               "Wj41xKQdYwny",
+	"authorization_endpoint": "https://usoraclegm1.storage.oraclecloud.com/auth/v1.0",
+}
+
+func TestStow(t *testing.T) {
+	test.All(t, "oracle", cfgMetered)
+}
+
+func TestGetItemUTCLastModified(t *testing.T) {
+	tr := http.DefaultTransport
+	http.DefaultTransport = &bogusLastModifiedTransport{tr}
+	defer func() {
+		http.DefaultTransport = tr
+	}()
+
+	test.All(t, "oracle", cfgMetered)
+}
+
+type bogusLastModifiedTransport struct {
+	http.RoundTripper
+}
+
+func (t *bogusLastModifiedTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	res, err := t.RoundTripper.RoundTrip(r)
+	if err != nil {
+		return res, err
+	}
+	res.Header.Set("Last-Modified", "Tue, 23 Aug 2016 15:12:44 UTC")
+	return res, err
+}
+
+func (t *bogusLastModifiedTransport) CloseIdleConnections() {
+	if tr, ok := t.RoundTripper.(interface {
+		CloseIdleConnections()
+	}); ok {
+		tr.CloseIdleConnections()
+	}
+}
+
+func TestPrepMetadataSuccess(t *testing.T) {
+	is := is.New(t)
+
+	m := make(map[string]string)
+	m["one"] = "two"
+	m["3"] = "4"
+	m["ninety-nine"] = "100"
+
+	m2 := make(map[string]interface{})
+	for key, value := range m {
+		m2[key] = value
+	}
+
+	assertionM := make(map[string]string)
+	assertionM["X-Object-Meta-one"] = "two"
+	assertionM["X-Object-Meta-3"] = "4"
+	assertionM["X-Object-Meta-ninety-nine"] = "100"
+
+	//returns map[string]interface
+	returnedMap, err := prepMetadata(m2)
+	is.NoErr(err)
+
+	if !reflect.DeepEqual(returnedMap, assertionM) {
+		t.Errorf("Expected map (%+v) and returned map (%+v) are not equal.", assertionM, returnedMap)
+	}
+}
+
+func TestPrepMetadataFailureWithNonStringValues(t *testing.T) {
+	is := is.New(t)
+
+	m := make(map[string]interface{})
+	m["float"] = 8.9
+	m["number"] = 9
+
+	_, err := prepMetadata(m)
+	is.Err(err)
+}
---
+++ swift/stow_test.go
@@ -0,0 +1,59 @@
+package swift
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/cheekybits/is"
+	"github.com/graymeta/stow"
+	"github.com/graymeta/stow/test"
+)
+
+func TestStow(t *testing.T) {
+	cfg := stow.ConfigMap{
+		"username":        "sek-chai",
+		"key":             "W33C6CNESJRE3H3FW67MLNNBERZRR4AGYDROX56LX2XWE4GQZC7YNS4CJEF4MPCYVYMJYPARZVUDNBOUI2XRRYWU4HTBPKQ4Q6NX",
+		"tenant_name":     "graymeta-demo-account",
+		"tenant_auth_url": "https://lax-01.identity.sohonet.com/v2.0/",
+		//"tenant_id":       "b04239c7467548678b4822e9dad96030",
+	}
+	test.All(t, "swift", cfg)
+}
+
+func TestPrepMetadataSuccess(t *testing.T) {
+	is := is.New(t)
+
+	m := make(map[string]string)
+	m["one"] = "two"
+	m["3"] = "4"
+	m["ninety-nine"] = "100"
+
+	m2 := make(map[string]interface{})
+	for key, value := range m {
+		m2[key] = value
+	}
+
+	assertionM := make(map[string]string)
+	assertionM["X-Object-Meta-one"] = "two"
+	assertionM["X-Object-Meta-3"] = "4"
+	assertionM["X-Object-Meta-ninety-nine"] = "100"
+
+	//returns map[string]interface
+	returnedMap, err := prepMetadata(m2)
+	is.NoErr(err)
+
+	if !reflect.DeepEqual(returnedMap, assertionM) {
+		t.Errorf("Expected map (%+v) and returned map (%+v) are not equal.", assertionM, returnedMap)
+	}
+}
+
+func TestPrepMetadataFailureWithNonStringValues(t *testing.T) {
+	is := is.New(t)
+
+	m := make(map[string]interface{})
+	m["float"] = 8.9
+	m["number"] = 9
+
+	_, err := prepMetadata(m)
+	is.Err(err)
+}
